c25fc972b38094c7a67f19cde3639bc7f0f1d6ce
Anton Domashnev <antondomashnev@gmail.com>
2017-03-12 23:22:38 +0100

    Add ApplicationSnapshotTestResultListenerSpec;

---
+++ FBSnapshotsViewerTests/ApplicationSnapshotTestResultListenerSpec.swift
@@ -0,0 +1,25 @@
+//
+//  ApplicationSnapshotTestResultListenerSpec.swift
+//  FBSnapshotsViewer
+//
+//  Created by Anton Domashnev on 11/03/2017.
+//  Copyright © 2017 Anton Domashnev. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import FBSnapshotsViewer
+
+class ApplicationSnapshotTestResultListener_MockFolderEventsListenerFactory: FolderEventsListenerFactory {
+    
+}
+
+class 
+
+class ApplicationSnapshotTestResultListenerSpec: QuickSpec {
+    override func spec() {
+        var snapshotTestResultListener: ApplicationSnapshotTestResultListener!
+        var folderEventsListenerFactory:
+    }
+}
