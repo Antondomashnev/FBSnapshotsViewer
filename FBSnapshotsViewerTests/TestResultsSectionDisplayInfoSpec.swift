5660f3c385af3be037ad8113a36bf73351712b7b
Anton Domashnev <antondomashnev@gmail.com>
2017-06-11 23:30:43 +0200

    Create TestResultsSectionDisplayInfoSpec;

---
+++ FBSnapshotsViewerTests/TestResultsSectionDisplayInfoSpec.swift
@@ -0,0 +1,36 @@
+//
+//  TestResultsSectionDisplayInfoSpec.swift
+//  FBSnapshotsViewerTests
+//
+//  Created by Anton Domashnev on 11.06.17.
+//  Copyright © 2017 Anton Domashnev. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import FBSnapshotsViewer
+
+class TestResultsSectionDisplayInfoSpec: QuickSpec {
+    override func spec() {
+        var displayInfo: TestResultsSectionDisplayInfo!
+        
+        describe(".init") {
+            var items: [TestResultDisplayInfo] = []
+            var title: TestResultsSectionTitleDisplayInfo!
+            
+            beforeEach {
+                let build = Build(date: Date(), applicationName: "MyApp")
+                let testResult = SnapshotTestResult.recorded(testName: "TestName", referenceImagePath: "foo/bar.png", build: build)
+                title = TestResultsSectionTitleDisplayInfo(build: build, testContext: "TestContext")
+                items = [TestResultDisplayInfo(testResult: testResult)]
+                displayInfo = TestResultsSectionDisplayInfo(title: title, items: items)
+            }
+            
+            it("initializes all properties") {
+                expect(displayInfo.itemInfos).to(equal(items))
+                expect(displayInfo.titleInfo).to(equal(title))
+            }
+        }
+    }
+}
