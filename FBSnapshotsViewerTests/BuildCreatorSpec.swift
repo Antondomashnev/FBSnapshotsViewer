3c39524356d9497c655a08abf2d1ec4f3293bb96
Anton Domashnev <antondomashnev@gmail.com>
2017-06-24 20:31:04 +0200

    Create BuildCreatorSpec;

--- FBSnapshotsViewerTests/BuildCreatorSpec.swift
+++ FBSnapshotsViewerTests/BuildCreatorSpec.swift
@@ -13,5 +13,50 @@ import Nimble
 
 class BuildCreatorSpec: QuickSpec {
     override func spec() {
+        var buildCreator: BuildCreator!
+        
+        beforeEach {
+            buildCreator = BuildCreator()
+        }
+        
+        describe(".createBuild") {
+            context("given application name, date and fbReferenceImageDirectoryURL") {
+                var date: Date!
+                var applicationName: String!
+                var fbReferenceImageDirectoryURL: URL!
+                
+                beforeEach {
+                    fbReferenceImageDirectoryURL = URL(fileURLWithPath: "/Users/fedotiy/myproject/tests/")
+                    applicationName = "MyApplication"
+                    date = Date()
+                    buildCreator.applicationName = applicationName
+                    buildCreator.fbReferenceImageDirectoryURL = fbReferenceImageDirectoryURL
+                    buildCreator.date = date
+                }
+                
+                it("returns build") {
+                    let expectedBuild = Build(date: date, applicationName: applicationName, fbReferenceImageDirectoryURL: fbReferenceImageDirectoryURL)
+                    expect(buildCreator.createBuild()).to(equal(expectedBuild))
+                }
+            }
+            
+            context("given not all properties") {
+                var date: Date!
+                var applicationName: String!
+                var fbReferenceImageDirectoryURL: URL!
+                
+                beforeEach {
+                    fbReferenceImageDirectoryURL = URL(fileURLWithPath: "/Users/fedotiy/myproject/tests/")
+                    applicationName = "MyApplication"
+                    date = Date()
+                    buildCreator.fbReferenceImageDirectoryURL = fbReferenceImageDirectoryURL
+                    buildCreator.date = date
+                }
+                
+                it("doesn't create build") {
+                    expect(buildCreator.createBuild()).to(beNil())
+                }
+            }
+        }
     }
 }
